d90dd86ffa0549afcfe4439207bb80cc899af1ae
Kepha Kibet <kibet.kepha@live.com>
2023-10-21 09:07:04 +0300

    method to get weather from api

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Weather } from './weather';
 @Injectable({
  providedIn: 'root'
 })
@@ -9,4 +11,14 @@ export class WeatherService {
   
   private apiUrl = 'https://api.openweathermap.org/data/2.5/';
   private apiKey = '<8c3bffa616ced0d0310632448b6f73a7';
+
+  getWeather(city: string): Observable<Weather> {
+    const options = new HttpParams()
+    .set('units', 'metric')
+    .set('q', city)
+    .set('appId', this.apiKey);
+    return this.http.get<Weather>(this.apiUrl + 'weather', { params:
+   options });
+  }
+  
 }
