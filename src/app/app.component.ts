1e36c3f190530b6b69c5b380cc730536ea140ecd
Kepha Kibet <kibet.kepha@live.com>
2023-10-22 18:07:13 +0300

    config snack bar and swupdate service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { filter, map, switchMap } from 'rxjs';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PWA-Weather-Application-Using-Angular-Service-Worker';
+
+  constructor(private updates: SwUpdate, private snackbar:
+    MatSnackBar) { }
+  
+    ngOnInit() {
+      this.updates.versionUpdates.pipe(
+      filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+      switchMap(() => this.snackbar.open('A new version isavailable!', 'Update now').afterDismissed()),
+      filter(result => result.dismissedByAction),
+      map(() => this.updates.activateUpdate().then(() => location.
+     reload()))
+      ).subscribe();
+     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WeatherComponent } from './weather/weather.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
+
 
 @NgModule({
   declarations: [
@@ -24,6 +26,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     MatIconModule,
     MatInputModule,
     MatCardModule,
+    MatSnackBarModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
